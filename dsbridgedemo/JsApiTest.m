9e8e51f10c9bb8e945e5dcaf738030d34eb2ee64
Jerry <chen.developer@foxmail.com>
2021-10-18 16:31:11 +0800

    Revert "test"

--- dsbridgedemo/JsApiTest.m
+++ dsbridgedemo/JsApiTest.m
@@ -1,5 +1,6 @@
 //
 //  JsApiTest.m
+//  dspider
 //
 //  Created by 杜文 on 16/12/30.
 //  Copyright © 2016年 杜文. All rights reserved.
@@ -7,23 +8,16 @@
 
 #import "JsApiTest.h"
 
-@interface JsApiTest(){
-  NSTimer * timer ;
-  void(^hanlder)(id value,BOOL isComplete);
-  int value;
-}
-@end
-
 @implementation JsApiTest
 
-- (NSString *) testSyn: (NSString *) msg
+- (NSString *) testSyn:(NSDictionary *) args
 {
-    return [msg stringByAppendingString:@"[ syn call]"];
+    return [(NSString *)[args valueForKey:@"msg"] stringByAppendingString:@"[ syn call]"];
 }
 
-- (void) testAsyn:(NSString *) msg :(JSCallback) completionHandler
+- (void) testAsyn:(NSDictionary *) args :(void (^)(NSString * _Nullable result))completionHandler
 {
-    completionHandler([msg stringByAppendingString:@" [ asyn call]"],YES);
+    completionHandler([(NSString *)[args valueForKey:@"msg"] stringByAppendingString:@"[ asyn call]"]);
 }
 
 - (NSString *)testNoArgSyn:(NSDictionary *) args
@@ -31,39 +25,10 @@
     return  @"testNoArgSyn called [ syn call]";
 }
 
-- ( void )testNoArgAsyn:(NSDictionary *) args :(JSCallback)completionHandler
+- ( void )testNoArgAsyn:(NSDictionary *) args :(void (^)(NSString * _Nullable result))completionHandler
 {
-    completionHandler(@"testNoArgAsyn called [ asyn call]",YES);
-}
-
-- ( void )callProgress:(NSDictionary *) args :(JSCallback)completionHandler
-{
-    value=10;
-    hanlder=completionHandler;
-    timer =  [NSTimer scheduledTimerWithTimeInterval:1.0
-                                              target:self
-                                            selector:@selector(onTimer:)
-                                            userInfo:nil
-                                             repeats:YES];
-}
-
--(void)onTimer:t{
-    if(value!=-1){
-        hanlder([NSNumber numberWithInt:value--],NO);
-    }else{
-        hanlder(0,YES);
-        [timer invalidate];
-    }
-}
-
-/**
- * Note: This method is for Fly.js
- * In browser, Ajax requests are sent by browser, but Fly can
- * redirect requests to native, more about Fly see  https://github.com/wendux/fly
- * @param requestInfo passed by fly.js, more detail reference https://wendux.github.io/dist/#/doc/flyio-en/native
- */
--(void)onAjaxRequest:(NSDictionary *) requestInfo  :(JSCallback)completionHandler{
-   
+    completionHandler(@"testNoArgAsyn called [ asyn call]");
+    //return nil;
 }
 
 @end
